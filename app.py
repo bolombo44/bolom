8bdbdbf3ef666fbc5321a023a5a6ad6570bab6a8
bolombo44 <bolomras@gmail.com>
2025-11-21 03:11:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def full_stripe_check(cc, mm, yy, cvv):
     session = requests.Session()
     session.headers.update({
-        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
     })
 
     if len(yy) == 4:
